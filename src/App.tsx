fb511f3f50b66bcbde8e6cb52aad4220068d17a9
Negima1072 <ngm1072@gmail.com>
2023-02-28 23:40:54 +0900

    add: not found

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./components/main/MainPage";
 import Layout from "./Layout";
 
+const NotFoundError = () => {
+  return (
+    <>
+      <h1>ページが存在しません</h1>
+    </>
+  )
+};
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
+          <Route path="/*" element={<NotFoundError />}/>
         </Route>
       </Routes>
     </BrowserRouter>
