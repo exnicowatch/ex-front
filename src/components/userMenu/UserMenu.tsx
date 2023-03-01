42ef995e607752e1354efe5e5e53e4534cefd04b
Negima1072 <ngm1072@gmail.com>
2023-03-01 22:32:56 +0900

    add: header component

---
+++ src/components/userMenu/UserMenu.tsx
@@ -0,0 +1,30 @@
+import { Popover } from "@mui/material";
+import React from "react";
+
+interface UserMenuProps{
+  isOpen: boolean,
+  onClose: () => void,
+  anchorEl: HTMLButtonElement | null
+}
+
+const UserMenu = (props: UserMenuProps) => {
+  return (
+    <Popover
+      open={props.isOpen}
+      anchorEl={props.anchorEl}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      onClose={props.onClose}
+    >
+      The content of the Popover.
+    </Popover>
+  )
+}
+
+export default UserMenu;
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+import { AppBar, Avatar, Container, IconButton, Toolbar, Typography } from "@mui/material";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { NicoContext } from "../provider/NicoProvider";
+import DehazeIcon from '@mui/icons-material/Dehaze';
 import Styled from "./Header.module.scss";
+import UserMenu from "../components/userMenu/UserMenu";
 
 const Header = () => {
-  return <header className={Styled.header}>header</header>;
+  const nicoContextValue = useContext(NicoContext);
+  const [openUserMenu, setOpenUserMenu] = useState(false);
+  const [userMenuAnchorEl, setUserMenuAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const handleUserMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setOpenUserMenu(true);
+    setUserMenuAnchorEl(event.currentTarget);
+  };
+  const handleUserMenuClose = () => {
+    setOpenUserMenu(false);
+    setUserMenuAnchorEl(null);
+  }
+  return (
+    <AppBar position="static" className={Styled.header}>
+      <Container className={Styled.headerCnt}>
+        <Toolbar disableGutters className={Styled.headerBar}>
+          <IconButton>
+            <DehazeIcon />
+          </IconButton>
+          <Typography
+            className={Styled.logoTitle}
+            variant="h5"
+            noWrap
+            component={Link}
+            to="/"
+          >
+            ExNicoWatch
+          </Typography>
+          <div style={{ flexGrow: 1 }}></div>
+          <button className={Styled.iconBtn} onClick={handleUserMenuClick}>
+            <Avatar src={nicoContextValue.loginUser?.icons.large} alt="UserIcon" />
+          </button>
+          <UserMenu anchorEl={userMenuAnchorEl} isOpen={openUserMenu} onClose={handleUserMenuClose} />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
