3737a07ca9f2070e6a012e09f01a5f193e6b9416
Negima1072 <ngm1072@gmail.com>
2023-03-01 22:31:44 +0900

    mv: main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainPage from "./components/main/MainPage";
+import MainPage from "./pages/main/MainPage";
 import Layout from "./Layout";
 
 const NotFoundError = () => {
