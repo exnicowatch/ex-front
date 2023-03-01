c307c3e734faffc5ed9c22e334d26e4d2188af5a
Negima1072 <ngm1072@gmail.com>
2023-03-02 01:17:41 +0900

    dev: main page tmp

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Styled from "./MainPage.module.scss";
 
 const MainPage = () => {
-  return <div className={Styled.mainPage}>MainPage</div>;
+  return <div className={Styled.mainPage}>
+    MainPage
+    <ul>
+      <li>
+        <Link to="/watch/sm9">sm9</Link>
+      </li>
+      <li>
+        <Link to="/watch/lv340461518">lv340461518</Link>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default MainPage;
