3f8e54673370629c4bcf6f419fd999e4f64d0b63
Negima1072 <ngm1072@gmail.com>
2023-03-02 19:39:51 +0900

    dev: main page tmp

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -10,7 +10,16 @@ const MainPage = () => {
         <Link to="/watch/sm9">sm9</Link>
       </li>
       <li>
+        <Link to="/watch/sm37198633">sm37198633</Link>
+      </li>
+      <li>
+        <Link to="/watch/sm41551011">sm41551011</Link>
+      </li>
+      <li>
         <Link to="/watch/lv340461518">lv340461518</Link>
+      </li>
+      <li>
+        <Link to="/watch/test">test</Link>
       </li>
     </ul>
   </div>;
